6c5c65ba425b208b6f4bd7d2919ba60fe20a9d18
Daniela Riedl <makkelaig@gmail.com>
2021-01-15 11:53:45 +0100

    refactored motor control functions

--- source/main.cpp
+++ source/main.cpp
@@ -301,69 +301,66 @@ unsigned char set_direction(ManagedString direction, unsigned char *bitmasks){
 }
 
 /* choose your board or write your own motor control */
-//Waveshare Motor Driver for micro:bit
 void setMotorPins(ManagedString msg) {
-    // Motor A in1 = pin 13,    Motor A in2 = pin 12
-    // Motor B in1 = pin 14,    Motor B in2 = pin 15
-    // Motor A PWM = pin 8,     Motor B PWM = pin 16
-    // bitmask for motor control: 0 0 P16 P15 P14 P8 P12 P13
-    // bitmasks for motorPins
-    unsigned char m1_pin1 = 1, m1_pin2 = 2, m1_pwm = 4, m2_pin1 = 8, m2_pin2 = 16, m2_pwm = 32;
-    // bitmasks for directions:
-    unsigned char bitmasks[6] = {45,54,5,40,53,46};
+    /**
+     * Waveshare Motor Driver Board for micro:bit
+     * Motor A in1 = pin 13,    Motor A in2 = pin 12
+     * Motor B in1 = pin 14,    Motor B in2 = pin 15
+     * Motor A PWM = pin 8,     Motor B PWM = pin 16
+     * bitmask for motor control: 0 0 P16 P15 P14 P8 P12 P13
+     */
+    unsigned char m1_pin1 = 1, m1_pin2 = 2, m1_pwm = 4, m2_pin1 = 8, m2_pin2 = 16, m2_pwm = 32; // bitmasks for motorPins
+    unsigned char bitmasks[6] = {45,54,5,40,53,46}; // bitmasks for directions
     ManagedString direction(msg.charAt(1));
     unsigned char moveMask = 0;
     moveMask = set_direction(direction,bitmasks);
     //set the pin values according to direction
-    uBit.io.P13.setDigitalValue(m1_pin1 & moveMask);                //dir motor1
+    uBit.io.P13.setDigitalValue(m1_pin1 & moveMask);                // dir motor1
     uBit.io.P12.setDigitalValue((m1_pin2 & moveMask)/m1_pin2);
-    uBit.io.P14.setDigitalValue((m2_pin1 & moveMask)/m2_pin1);      //dir motor2
+    uBit.io.P14.setDigitalValue((m2_pin1 & moveMask)/m2_pin1);      // dir motor2
     uBit.io.P15.setDigitalValue((m2_pin2 & moveMask)/m2_pin2);
-    //if((P8.setAnalogValue(((m1_pwm & moveMask)/m1_pwm) * velocity)) != MICROBIT_OK){uBit.display.scroll("P8");}
-    P8.setAnalogValue(((m1_pwm & moveMask)/m1_pwm) * velocity_1);   //pwm motor1
-    P16.setAnalogValue(((m2_pwm & moveMask)/m2_pwm) * velocity_2);  //pwm motor2
+    P8.setAnalogValue(((m1_pwm & moveMask)/m1_pwm) * velocity_1);   // pwm motor1
+    P16.setAnalogValue(((m2_pwm & moveMask)/m2_pwm) * velocity_2);  // pwm motor2
 }
 
-// ElecFreaks Motor:bit Board
-/*
-void setMotorPins(ManagedString msg) {
-    // Motor 1 PWM = P1,    Motor1 direction = P8 (LOW = CC, HIGH = C)
-    // Motor 2 PWM = P2,    Motor2 direction = P12(LOW = CC, HIGH = C)
-    // bitmask motor control: X X X X P12 P2 P8 P1
-    // bitmasks for motor pins:
-    unsigned char m1_pwm = 1, m1_dir = 2, m2_pwm = 4, m2_dir = 8;
-    // bitmasks for directions:
-    unsigned char bitmasks[6] = {15,5,12,3,13,7};
-    ManagedString direction(msg.charAt(1));
-    unsigned char moveMask = set_direction(direction,bitmasks);
-    //set the pin values according to direction
-    uBit.io.P12.setDigitalValue((m2_dir & moveMask)/m2_dir);              //dir motor2
-    uBit.io.P8.setDigitalValue((m1_dir & moveMask)/m1_dir);               //dir motor1
-    uBit.io.P1.setAnalogValue((m1_pwm & moveMask)* velocity_1);             //pwm motor1
-    uBit.io.P2.setAnalogValue(((m2_pwm & moveMask)/m2_pwm) * velocity_2);   //pwm motor2
-}
-*/
+//void setMotorPins(ManagedString msg) {
+//    /**
+//     * ElecFreaks Motor:bit Board
+//     * Motor 1 PWM = P1,    Motor1 direction = P8 (LOW = CC, HIGH = C)
+//     * Motor 2 PWM = P2,    Motor2 direction = P12(LOW = CC, HIGH = C)
+//     * bitmask motor control: X X X X P12 P2 P8 P1
+//     * bitmasks for motor pins:
+//     */
+//    unsigned char m1_pwm = 1, m1_dir = 2, m2_pwm = 4, m2_dir = 8;
+//    unsigned char bitmasks[6] = {15,5,12,3,13,7};   // bitmasks for directions
+//    ManagedString direction(msg.charAt(1));
+//    unsigned char moveMask = set_direction(direction,bitmasks);
+//    // Set the pin values according to direction
+//    uBit.io.P12.setDigitalValue((m2_dir & moveMask)/m2_dir);              //dir motor2
+//    uBit.io.P8.setDigitalValue((m1_dir & moveMask)/m1_dir);               //dir motor1
+//    uBit.io.P1.setAnalogValue((m1_pwm & moveMask)* velocity_1);           //pwm motor1
+//    uBit.io.P2.setAnalogValue(((m2_pwm & moveMask)/m2_pwm) * velocity_2); //pwm motor2
+//}
 
-// Keyestudio Motor Driver Board v1.8
-/*
-void setMotorPins(ManagedString msg) {
-    // Enable = pin14
-    // Motor1 CW = pin12,   Motor1 CCW = pin13
-    // Motor2 CW = pin15,   Motor2 CCW = pin16
-    // Motor1 PWM = pin1,   Motor2 PWM = pin2
-    // Bitmask motor control: X X X P15 P16 P12 P13 P14
-    // bitmasks for directions:
-    unsigned char bitmasks[6] = {11,21,9,3,13,19};
-    ManagedString direction(msg.charAt(1));
-    unsigned char moveMask = set_direction(direction,bitmasks);
-    // set motor pins according to direction
-    uBit.io.P1.setAnalogValue(velocity_1);
-    uBit.io.P2.setAnalogValue(velocity_2);
-    uBit.io.P12.setDigitalValue((4 & moveMask)/4);
-    uBit.io.P13.setDigitalValue((2 & moveMask)/2);
-    uBit.io.P15.setDigitalValue((16 & moveMask)/16);
-    uBit.io.P16.setDigitalValue((8 & moveMask)/8);
-    uBit.io.P14.setDigitalValue(1 & moveMask);
-}
-*/
+//void setMotorPins(ManagedString msg) {
+//    /**
+//     * Keyestudio Motor Driver Board v1.8
+//     * Enable = pin14
+//     * Motor1 CW = pin12,   Motor1 CCW = pin13
+//     * Motor2 CW = pin15,   Motor2 CCW = pin16
+//     * Motor1 PWM = pin1,   Motor2 PWM = pin2
+//     * Bitmask motor control: X X X P15 P16 P12 P13 P14
+//     */
+//    unsigned char bitmasks[6] = {11,21,9,3,13,19};  // bitmasks for directions
+//    ManagedString direction(msg.charAt(1));
+//    unsigned char moveMask = set_direction(direction,bitmasks);
+//    // set motor pins according to direction
+//    uBit.io.P1.setAnalogValue(velocity_1);
+//    uBit.io.P2.setAnalogValue(velocity_2);
+//    uBit.io.P12.setDigitalValue((4 & moveMask)/4);
+//    uBit.io.P13.setDigitalValue((2 & moveMask)/2);
+//    uBit.io.P15.setDigitalValue((16 & moveMask)/16);
+//    uBit.io.P16.setDigitalValue((8 & moveMask)/8);
+//    uBit.io.P14.setDigitalValue(1 & moveMask);
+//}
 
